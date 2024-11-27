f3066f13b0c84930080a745affc6038953f621de
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2024-11-27 13:51:10 +0100

    First try for safe calculator Co-authored-by: Julien Klein <iLdro@users.noreply.github.com> Co-authored-by: Adam N'MILY <BowlFullOfPineapples@users.noreply.github.com>

--- java/src/test/java/info/dmerej/DiscountApplierTest.java
+++ java/src/test/java/info/dmerej/DiscountApplierTest.java
@@ -1,11 +1,23 @@
 package info.dmerej;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class DiscountApplierTest {
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v1() {
     // TODO: write a test to demonstrate the bug in DiscountApplier.applyV1()
+
+    //Given 
+    
+    List<User> users = new ArrayList<User>();
+
+    //When
+
+
+    //Then
   }
 
   @Test
--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -1,10 +1,33 @@
 package info.dmerej;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
     // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+
+    //Given
+    Authorizer authorizer = new Authorizer() {
+      @Override
+      public boolean authorize() {
+        return true;
+      }
+    };
+    SafeCalculator safeCalculator = new SafeCalculator(authorizer);
+
+    //When
+    
+    int sum = safeCalculator.add(1, 1);
+
+
+    //Then
+
+    //assert sum == 2;
+    assertEquals(sum, 2);
+
+
   }
 }
