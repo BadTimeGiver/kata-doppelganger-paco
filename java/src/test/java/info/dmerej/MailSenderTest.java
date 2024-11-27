e1c40868d8238438e1a3b22ada8234a4e2945262
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2024-11-27 16:10:49 +0100

    Implemented Mail Sender tests

--- java/src/test/java/info/dmerej/MailSenderTest.java
+++ java/src/test/java/info/dmerej/MailSenderTest.java
@@ -6,10 +6,35 @@ public class MailSenderTest {
     @Test
     void should_make_a_valid_http_request() {
         // TODO: write a test to demonstrate the bug in MailSender.sendV1()
+
+        //Given
+        EmailHttpClient emailHttpClient = new EmailHttpClient();
+
+        MailSender mailSender = new MailSender(emailHttpClient);
+        User user = new User("Zyad", "Zyad@mail.com");
+
+
+        //When
+        mailSender.sendV1(user,"Hi");
+
+        //Then
+        assertEquals(200, emailHttpClient.getMailResponse().code());
     }
 
     @Test
     void should_retry_when_getting_a_503_error() {
         // TODO: write a test to demonstrate the bug in MailSender.sendV2()
+
+        //Given
+        EmailHttpClient emailHttpClient = new EmailHttpClient();
+
+        MailSender mailSender = new MailSender(emailHttpClient);
+        User user = new User("Zyad", "Zyad@mail.com");
+
+        //When
+        mailSender.sendV2(user,"");
+
+        //Then
+        assertEquals(200, emailHttpClient.getMailResponse().code());
     }
 }
